74ab024d7c4477c75438c2ed6f58976d0c0fc166
mert0119 <barutcu.462@gmail.com>
2026-02-20 11:08:32 +0300

    Render DB yolu duzeltmesi + logo guncelleme

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'spor-diyet-app-gizli-anahtar-2024'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fitol-gizli-anahtar-2026'
+    # Render'da /tmp kullan, lokalde instance/ klasörü
+    if os.environ.get('RENDER'):
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
