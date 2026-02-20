92f9ee34d0866573865423c0d3ad20ba60ed179e
mert0119 <barutcu.462@gmail.com>
2026-02-20 11:14:17 +0300

    wsgi.py duzeltme - sys.path eklendi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+# Render icin calisma dizinini ayarla
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app import create_app
 
 app = create_app()
+
+if __name__ == '__main__':
+    app.run()
